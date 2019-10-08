98521986fe25731017c1079d7374e245eadfe29a
judinilson <judinilson0000@gmail.com>
2019-10-08 10:48:17 +0400

    creating client table with material examples and reformat our material import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
+//import { MatFormFieldModule} from '@angular/material/form-field'
 import { ReportPageComponent } from './report-page/report-page.component';
 import { ClientListPageComponent } from './client-list-page/client-list-page.component';
+
+//Angular material modules 
+import {
+  MatTableModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatPaginatorModule 
+ } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -26,11 +40,17 @@ import { ClientListPageComponent } from './client-list-page/client-list-page.com
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
+
+    //material modules
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule ,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
