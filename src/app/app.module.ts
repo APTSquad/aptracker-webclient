1e1aa88b2830a3897cc054cda1f88f270fd1d9e6
D5T <cyanogen.gg@gmail.com>
2019-10-09 22:24:24 +0400

    Don't even ask me what I did

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { ReportPageComponent } from './report-page/report-page.component';
-import { ClientListPageComponent } from './client-list-page/client-list-page.component';
+import { ReportPageComponent } from './pages/report-page/report-page.component';
+import { ClientListPageComponent } from './pages/client-list-page/client-list-page.component';
+import { ThemePickerModule } from './shared/theme-picker';
+import { NavBarModule } from './shared/navbar/navbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     ReportPageComponent,
-    ClientListPageComponent
+    ClientListPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,7 +30,9 @@ import { ClientListPageComponent } from './client-list-page/client-list-page.com
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    ThemePickerModule,
+    NavBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/configuration/pages.ts
@@ -0,0 +1,32 @@
+export interface NavLinkInfo {
+    name: string;
+    location: string;
+}
+
+export const ROLE_DEVELOPER = "DEVELOPER";
+export const ROLE_MANAGER = "MANAGER";
+export const ROLE_ADMIN = "ADMIN";
+
+export const PAGE_REPORT = "REPORT";
+export const PAGE_PAST_REPORTS = "PAST_REPORTS";
+export const PAGE_CLIENTS = "CLIENTS";
+export const PAGE_MANAGER_REPORTS = "MANAGER_REPORTS";
+
+export const NAV_LINKS: { [key: string]: NavLinkInfo } = {
+    [PAGE_REPORT]: {
+        name: 'Ежедневный отчет', location: '/report'
+    },
+    [PAGE_PAST_REPORTS]: {
+        name: 'Прошлые отчеты', location: '/pastReports'
+    },
+    [PAGE_CLIENTS]: {
+        name: 'Клиенты', location: '/clients'
+    },
+
+}
+
+export const NAV_SECTIONS: { [key: string]: Array<string> } = {
+    [ROLE_DEVELOPER]: [PAGE_REPORT, PAGE_PAST_REPORTS],
+    [ROLE_MANAGER]: [PAGE_MANAGER_REPORTS],
+    [ROLE_ADMIN]: [PAGE_REPORT, PAGE_CLIENTS]
+};
