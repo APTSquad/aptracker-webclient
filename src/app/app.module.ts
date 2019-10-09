5e48506c8c2263f313d856471999938ebad0bd30
judinilson <judinilson0000@gmail.com>
2019-10-09 13:18:33 +0400

    rearrange table with simple examples for UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatSidenavModule } from '@angular/material/sidenav';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatListModule } from '@angular/material/list';
-//import { MatFormFieldModule} from '@angular/material/form-field'
 import { ReportPageComponent } from './report-page/report-page.component';
 import { ClientListPageComponent } from './client-list-page/client-list-page.component';
 
--- src/app/client-list-page/client-list-page.component.ts
+++ src/app/client-list-page/client-list-page.component.ts
@@ -5,19 +5,32 @@ import {MatTableDataSource} from '@angular/material/table';
 
 export interface UserData {
   id: string;
-  name: string;
-  progress: string;
-  color: string;
+  client: string;
+  projects: string;
+  article: string;
 }
 
 /** Constants used to fill up our data base. */
-const COLORS: string[] = [
+const PROJECTS: string[] = [
   'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
   'aqua', 'blue', 'navy', 'black', 'gray'
 ];
-const NAMES: string[] = [
+const CLIENT_NAMES: string[] = [
   'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
   'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
+
+const ARTICLES: string[] = [
+  'From its medieval origins to the digital', 
+  'aying out print, graphic or web designs. ',
+   'unknown typesetter in the 15th century ', 
+   'scrambled parts of Ciceros De Finibus Bonorum', 
+   'Lorem ipsum dolor sit amet, consectetur', 
+   'The purpose of lorem ipsum is to create', 
+   'The passage experienced a surge ', 
+   'Latin derived from', 
+   'Until recently, the prevailing view', 
+   'Um, not so fast',
 ];
 
 @Component({
@@ -27,15 +40,15 @@ const NAMES: string[] = [
 })
 export class ClientListPageComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
+  displayedColumns: string[] = ['id', 'client', 'project', 'article'];
   dataSource: MatTableDataSource<UserData>;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    // Create 50 users
+    const users = Array.from({length: 50}, (_, k) => createNewUser(k + 1));
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
@@ -57,14 +70,14 @@ export class ClientListPageComponent implements OnInit {
 
 /** Builds and returns a new User. */
 function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+  const name = CLIENT_NAMES[Math.round(Math.random() * (CLIENT_NAMES.length - 1))] + ' ' +
+  CLIENT_NAMES[Math.round(Math.random() * (CLIENT_NAMES.length - 1))].charAt(0) + '.';
 
   return {
     id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    client: name,
+    projects: PROJECTS[Math.round(Math.random() * (PROJECTS.length - 1))],
+    article: ARTICLES[Math.round(Math.random() * (ARTICLES.length - 1))]
   };
 
 }
