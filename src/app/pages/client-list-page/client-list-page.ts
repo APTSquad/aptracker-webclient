3ed511d42d2dc83c6ccddf03d0ff441b4ac4afd1
judinilson <judinilson0000@gmail.com>
2019-10-17 03:44:26 +0400

    change the table ui and create a new

--- src/app/pages/client-list-page/client-list-page.ts
+++ src/app/pages/client-list-page/client-list-page.ts
@@ -2,89 +2,156 @@ import { Component, OnInit, ViewChild, NgModule } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {CommonModule} from '@angular/common';
 import {
   MatIconModule,
   MatListModule,
   MatFormFieldModule,
   MatInputModule
 } from '@angular/material';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
-export interface UserData {
-  client: string;
-  projects: string;
-  article: string;
-}
-
-/** Constants used to fill up our data base. */
-const PROJECTS: string[] = [
-  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-  'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const CLIENT_NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
-
-const ARTICLES: string[] = [
-  'From its medieval origins to the digital',
-  'aying out print, graphic or web designs. ',
-  'unknown typesetter in the 15th century ',
-  'scrambled parts of Ciceros De Finibus Bonorum',
-  'Lorem ipsum dolor sit amet, consectetur',
-  'The purpose of lorem ipsum is to create',
-  'The passage experienced a surge ',
-  'Latin derived from',
-  'Until recently, the prevailing view',
-  'Um, not so fast',
-];
 
 @Component({
   selector: 'app-client-list-page',
   templateUrl: './client-list-page.html',
-  styleUrls: ['./client-list-page.scss']
+  styleUrls: ['./client-list-page.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ClientListPageComponent implements OnInit {
 
-  displayedColumns: string[] = ['client', 'project', 'article'];
-  dataSource: MatTableDataSource<UserData>;
+  dataSource = ELEMENT_DATA;
 
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  columnsToDisplay = ['No','name'];
+  expandedElement: PeriodicElement | null;
+
+  // dataSource: MatTableDataSource<UserData>;
+
+  // @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  // @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor() {
-    // Create 50 users
-    const users = Array.from({ length: 50 }, (_) => createNewUser());
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    //this.dataSource.paginator = this.paginator;
+    //this.dataSource.sort = this.sort;
   }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+//   applyFilter(filterValue: string) {
+//     this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+//     if (this.dataSource.paginator) {
+//       this.dataSource.paginator.firstPage();
+//     }
+//   }
+ }
+
+
+export interface PeriodicElement {
+  No:number;
+  name: string;
+  projects:string[];
+  //description: string;
 }
 
-/** Builds and returns a new User. */
-function createNewUser(): UserData {
-  const name = CLIENT_NAMES[Math.round(Math.random() * (CLIENT_NAMES.length - 1))] + ' ' +
-    CLIENT_NAMES[Math.round(Math.random() * (CLIENT_NAMES.length - 1))].charAt(0) + '.';
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    No: 1,
+    name: 'Hydrogen',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  }, {
+    No: 2,
+    name: 'Helium',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+  }, {
+    No: 3,
+    name: 'Lithium',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+  }, {
+    No: 4,
+    name: 'Beryllium',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+  }, {
+    No: 5,
+    name: 'Boron',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+  }, {
+    No: 6,
+    name: 'Carbon',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  }, {
+    No: 7,
+    name: 'Nitrogen',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  }, {
+    No: 8,
+    name: 'Oxygen',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  }, {
+    No: 9,
+    name: 'Fluorine',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  }, {
+    No: 10,
+    name: 'Neon',
+    projects: [
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project',
+      ' Hydrogen is a chemical element with project'
+    ]
+    
+  },
+];
 
-  return {
-    client: name,
-    projects: PROJECTS[Math.round(Math.random() * (PROJECTS.length - 1))],
-    article: ARTICLES[Math.round(Math.random() * (ARTICLES.length - 1))]
-  };
-
-}
 
 @NgModule({
   imports: [
@@ -93,7 +160,8 @@ function createNewUser(): UserData {
     MatTableModule,
     MatFormFieldModule,
     MatPaginatorModule,
-    MatInputModule
+    MatInputModule,
+    CommonModule
   ],
   exports: [ClientListPageComponent],
   declarations: [ClientListPageComponent],
