38b56c8e831bb1682e1334afd7918a839fbc437b
Alisher <brexceed@gmail.com>
2019-10-18 10:35:53 +0400

    Add treetable

--- src/app/pages/report-page/report-page.component.ts
+++ src/app/pages/report-page/report-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { TreetableModule, Node } from '../../treetable/treetable.module';
+import { mockTree } from '../../treetable/mocks/mockTree';
+import { mockTreeAsArrayOfNodes } from '../../treetable/mocks/mockTreeAsArrayOfNodes';
+import { Folder, Task } from '../../treetable/mocks/models';
+
 
 @Component({
   selector: 'app-report-page',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportPageComponent implements OnInit {
 
+  singleRootTree: Node<Folder> = mockTree;
+  arrayOfNodesTree: Node<Task>[] = mockTreeAsArrayOfNodes;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+@NgModule({
+  imports: [
+    TreetableModule
+  ],
+  exports: [ReportPageComponent],
+  declarations: [ReportPageComponent],
+})
+export class ReportPageModule { }
