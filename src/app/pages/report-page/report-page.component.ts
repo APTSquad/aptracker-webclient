0c8b83acb31bd01b3697710e4cca3cf8b78cb9be
Alisher <brexceed@gmail.com>
2019-11-10 14:09:34 +0400

    base

--- src/app/pages/report-page/report-page.component.ts
+++ src/app/pages/report-page/report-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatListModule } from "@angular/material/list";
+import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
@@ -9,7 +9,7 @@ import { MatInputModule } from '@angular/material/input';
   styleUrls: ['./report-page.component.scss']
 })
 export class ReportPageComponent implements OnInit {
-  items=DATA;
+  items = DATA;
   constructor() { }
 
   ngOnInit() {
@@ -19,20 +19,20 @@ export class ReportPageComponent implements OnInit {
 
 const DATA = [
   {
-    name: "Клиент1", 
-    projects:[
-      {name: "Проект1", expenses:[{name:"Тестирование"}, {name:"Администрирование"}, {name:"Затрата 3"}]},
-      {name: "Проект2", expenses:[{name:"Затрата 1"}, {name:"Затрата 2"}]},
-      {name: "Проект3", expenses:[{name:"Затрата 1"}, {name:"Затрата 2"}, {name:"Затрата 3"}]},
+    name: 'Клиент1',
+    projects: [
+      {name: 'Проект1', expenses: [{name: 'Тестирование'}, {name: 'Администрирование'}, {name: 'Затрата 3'}]},
+      {name: 'Проект2', expenses: [{name: 'Затрата 1'}, {name: 'Затрата 2'}]},
+      {name: 'Проект3', expenses: [{name: 'Затрата 1'}, {name: 'Затрата 2'}, {name: 'Затрата 3'}]},
     ]
   }
   ,
   {
-    name: "Клиент2", 
-    projects:[
-      {name: "Проект1", expenses:[{name:"Затрата 1"}, {name:"Затрата 2"}]},
-      {name: "Проект2", expenses:[{name:"Затрата 1"}, {name:"Затрата 2"}, {name:"Затрата 3"}]},
-      {name: "Проект3", expenses:[{name:"Затрата 1"}]},
+    name: 'Клиент2',
+    projects: [
+      {name: 'Проект1', expenses: [{name: 'Затрата 1'}, {name: 'Затрата 2'}]},
+      {name: 'Проект2', expenses: [{name: 'Затрата 1'}, {name: 'Затрата 2'}, {name: 'Затрата 3'}]},
+      {name: 'Проект3', expenses: [{name: 'Затрата 1'}]},
     ]
   }
 ];
